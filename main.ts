6f9ebe52d28724ec28132d602deffac18ea203b3
joy410975021 <71752080+joy410975021@users.noreply.github.com>
2020-09-30 15:31:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let grade = 0
+input.onGesture(Gesture.Shake, function () {
+    grade = randint(80, 100)
+    basic.showNumber(grade)
+    if (grade < 84) {
+        basic.showString("A-")
+    } else if (grade < 89) {
+        basic.showString("A")
+    } else if (grade < 100) {
+        basic.showString("A+")
+    }
+})
+basic.forever(function () {
+	
+})
